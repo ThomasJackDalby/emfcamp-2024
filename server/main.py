9fd2ff15fc72397f66aeb6ea3fd0b4316c0ef676
Tom Dalby <thomasjackdalby@gmail.com>
2024-05-31 13:01:15 +0100

    updating log files

--- server/main.py
+++ server/main.py
@@ -6,7 +6,7 @@ from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
-logging.basicConfig(filename="weblogs.txt")
+logging.basicConfig(filename="~/emfcamp-2024/server/static/fastapi.log")
 
 logger = logging.getLogger(__name__)
 
