364e036f9c032a06465628d49862f09cd40b0d7a
Tom Dalby <thomasjackdalby@gmail.com>
2024-05-31 12:45:36 +0100

    more logging

--- server/launch.sh
+++ server/launch.sh
@@ -7,4 +7,4 @@ git pull
 cd ./server
 chmod 755 launch.sh
 .venv/bin/pip install -r requirements.txt
-.venv/bin/fastapi run main.py
+.venv/bin/uvicorn main:app --log-config /home/fish/emfcamp-2024/server/log.ini
