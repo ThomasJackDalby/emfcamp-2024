a5c8d3fc5018fa3222d159e04c0904569289b6b4
Tom Dalby <thomasjackdalby@gmail.com>
2024-06-02 08:08:47 +0100

    updating site

--- server/static/script.js
+++ server/static/script.js
@@ -2,45 +2,64 @@ function sendMessage() {
 
     let name = document.getElementById("name").value;
     let message = document.getElementById("message").value;
+    let p = Printer()
 
-    let data = {
-        styles: [
-            {
-                "double_height": false,
-                "double_width": false,
-                "bold": false,
-                "align": "left",
-                "underline": false
-            },
-            {
-                "double_height": true,
-                "double_width": true,
-                "bold": true,
-                "align": "center",
-                "underline": true
-            },
-            {
-                "double_height": false,
-                "double_width": false,
-                "bold": false,
-                "align": "center",
-                "underline": false
-            }
-        ],
-        commands: [
-            { type: "text", content: "!! MESSAGE !!", style: 1 },
-            { type: "text", content: "-------------", style: 2 },
-            { type: "text", content: message, style: 0 },
-            { type: "text", content: "From "+name, style: 0 },
-            { type: "text", content: "-------------", style: 2 },
-        ]
-    };
+    p.style(
+        double_height=false,
+        double_width=false,
+        bold=false,
+        align="left",
+        underline=false
+    );
+    p.style(
+        double_height=true,
+        double_width=true,
+        bold=true,
+        align="center",
+        underline=true
+    );
+    p.style(
+        double_height=false,
+        double_width=false,
+        bold=false,
+        align="center",
+        underline=false
+    );
 
-    fetch("http://151.216.211.144:8000/api/print", {
+    p.text("!! MESSAGE !!", 1)
+    p.text("-------------", 2);
+    p.text(message);
+    p.text("From "+name);
+    p.text("-------------", 2);
+}
+
+class Printer
+{
+    constructor() {
+        this.commands = [];
+        this.styles = [];
+    }
+
+    text(text, style=0) {
+        this.commands.push({ type: "text", content: text, style: style });
+    }
+
+    flush() {
+        let request = {
+            "styles" : this.commands,
+            "commands" : this.styles,
+        }
+        fetch("http://151.216.211.144:8000/api/print", {
         method: "POST",
         headers: {'Content-Type': 'application/json'}, 
-        body: JSON.stringify(data)
+        body: JSON.stringify(request)
     }).then(res => {
         console.log("Request complete! response:", res);
     });
+    }
+}
+
+function getAles() {
+
+
 }
